b4e53d2e20b2860fc8cb2a3e4fd40593942cd1ae
stserhio <2411sergo@gmail.com>
2023-08-24 21:30:39 +0300

    нужно добавить рамки для общих ошибок

--- app/auth/registration/page.js
+++ app/auth/registration/page.js
@@ -5,19 +5,25 @@ import Modal from '../../../components/modal'
 import { useState } from 'react'
 import { redirect } from 'next/navigation'
 import { useRouter } from 'next/navigation'
+import { ArrowPathIcon} from '@heroicons/react/24/outline'
+
 
 export default function Registration() {
 
     const [showModal, setShowModal] = useState(false)
+
     const [firstName, setFirstName] = useState('')
     const [lastName, setLastName] = useState('')
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
-    const [errFirstName, setErrFirstName] = useState('eee')
+    const [errFirstName, setErrFirstName] = useState('')
     const [errLastName, setErrLastName] = useState('')
     const [errEmail, setErrEmail] = useState('')
     const [errPassword, setErrPassword] = useState('')
+
+    const [errMessage, setErrMessage] = useState('')
+    const [loading, setLoading] = useState(false)
 
 
 
@@ -28,9 +34,73 @@ export default function Registration() {
         router.push('/auth/login')
     }
 
-    const handlerRegistration = (e) => {
+    const handlerRegistration = async (e) => {
         e.preventDefault()
-        setShowModal(true);
+        setLoading(true)
+
+        const res = await fetch('/back/api/v1/register', {
+            method: 'POST',
+            headers: {
+                "Content-Type": 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                firstName,
+                lastName,
+                email,
+                password
+            })
+        })
+
+        if(res.status >=500){
+            setErrMessage('Извините, сервис недоступен. Повторите попытку позже')
+            setLoading(false)
+            return
+
+        }
+
+        const data = await res.json()
+
+        if(res.status === 400){
+            const { errors } = data
+            errors.forEach(err => {
+                switch (err.path) {
+                    case 'firstName':
+                        setErrFirstName(err.msg)
+                        break
+                    case 'lastName':
+                        setErrLastName(err.msg)
+                        break
+                    case 'email':
+                        setErrEmail(err.msg)
+                        break
+                    case 'password':
+                        setErrPassword(err.msg)
+                        break
+                    default:
+                        setErrMessage('Ошибка несуществующего поля')
+                        break
+                }
+            })
+            setLoading(false)
+            return
+         }
+
+        if (res.status === 409 || res.status === 418){
+            const {message} = data
+            setErrMessage(message)
+            setLoading(false)
+            return
+        }
+
+        if(res.status === 200){
+            setShowModal(true)
+            setLoading(false)
+            return
+        }
+
+
+
     }
 
 
@@ -46,7 +116,8 @@ export default function Registration() {
                     priority
                 />
                 <div className='lg:col-start-4 lg:col-end-6'>
-                    <div className='w-full rounded-lg bg-white dark:bg-slate-800 md:shadow-lg px-8 py-12'>
+                    <div className='w-full relative rounded-lg bg-white dark:bg-slate-800 md:shadow-lg px-8 py-12'>
+                        {errMessage && <div className='absolute right-8 left-8 text-red-500 border-red-500 rounded-md p-2 text-[10px] border-red-500 top-[20px]'>{errMessage}</div>}
                         <div className='flex items-center justify-center gap-2 mt-4'>
                             <Image
                                 src="/images/logo.svg"
@@ -59,49 +130,60 @@ export default function Registration() {
                             <span className='text-lg font-bold text-black dark:text-white'>Instagram</span>
                         </div>
                         <form className='grid mt-4 gap-6' onSubmit={handlerRegistration}>
-                            <div>
+                            <div className='relative w-full'>
                                 <input
-                                    className='w-full bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md'
+                                    className={`${errFirstName ? 'border-red-500 ' : 'border-transparent'} border bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md w-full`}
                                     type='text'
                                     name="firstName"
                                     placeholder='Имя'
                                     required={true}
                                     value={firstName}
-                                    onChange={(event)=> {setFirstName(event.target.value)}}/>
-                                {errFirstName && <div className='absolute text-red-500 text-[10px]'>Длина не должна быть более 30 и не менее 3 символов</div>}
+                                    onChange={(event)=> {setFirstName(event.target.value); setErrFirstName(''); setErrMessage('')}}/>
+                                {errFirstName && <div className='absolute border-red-500 text-red-500 text-[10px] -top-[16px]'>{errFirstName}</div>}
+                            </div>
+                            <div className='relative w-full'>
+                                <input
+                                    className={`${errLastName ? 'border-red-500 ' : 'border-transparent'} border bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md w-full`}
+                                    type='text'
+                                    name="lastName"
+                                    placeholder='Фамилия'
+                                    required={true}
+                                    value={lastName}
+                                    onChange={(event)=> {setLastName(event.target.value); setErrLastName(''); setErrMessage('')}}/>
+                                {errLastName && <div className='absolute border-red-500 text-red-500 text-[10px] -top-[16px]'>{errLastName}</div>}
                             </div>
 
-                            <input
-                                className='bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md'
-                                type='text'
-                                name="lastName"
-                                placeholder='Фамилия'
-                                required={true}
-                                value={lastName}
-                                onChange={(event)=> {setLastName(event.target.value)}}/>
-                            <input
-                                className='bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md'
-                                type='email'
-                                name="email"
-                                placeholder='example@email.com'
-                                required={true}
-                                value={email}
-                                onChange={(event)=> {setEmail(event.target.value)}}/>
-                            <input
-                                className='bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md'
-                                type='password'
-                                name="password"
-                                placeholder='Пароль'
-                                required={true}
-                                value={password}
-                                onChange={(event)=> {setPassword(event.target.value)}}/>
+                            <div className='relative w-full'>
+                                <input
+                                    className={`${errEmail ? 'border-red-500 ' : 'border-transparent'} border bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md w-full`}
+                                    type='email'
+                                    name="email"
+                                    placeholder='example@email.com'
+                                    required={true}
+                                    value={email}
+                                    onChange={(event)=> {setEmail(event.target.value); setErrEmail(''); setErrMessage('')}}/>
+                                {errEmail && <div className='absolute border-red-500 text-red-500 text-[10px] -top-[16px]'>{errEmail}</div>}
+                            </div>
+
+                            <div className='relative w-full'>
+                                <input
+                                    className={`${errPassword ? 'border-red-500 ' : 'border-transparent'} border bg-slate-100 dark:bg-slate-700 text-black dark:text-white py-2 px-4 rounded-md w-full`}
+                                    type='password'
+                                    name="password"
+                                    placeholder='Пароль'
+                                    required={true}
+                                    value={password}
+                                    onChange={(event)=> {setPassword(event.target.value); setErrPassword(''); setErrMessage('')}}/>
+                                {errPassword && <div className='absolute border-red-500 text-red-500 text-[10px] -top-[16px]'>{errPassword}</div>}
+                            </div>
+
                             <button
                                 className='scale-100 mt-4 w-full hover:scale-105 hover:drop-shadow-xl ease-in-out duration-300 py-3 px-4 rounded-md \
                             bg-gradient-to-r from-amber-500 dark:from-purple-600 from-0% via-orange-600 dark:via-cyan-600 via-30% via-pink-500 dark:via-blue-500 via-60% to-fuchsia-700 dark:to-violet-700 to-100% \
-                            text-white text-lg'
+                            text-white text-lg flex items-center justify-center'
                                 type='submit'
                             >
-                                Зарегистрироваться
+                                {loading ? <ArrowPathIcon className='w-6 h-6 animate-spin'/> : 'Зарегистрироваться' }
                             </button>
                         </form>
                         <div className='w-full border-b-2 border-slate-200 dark:border-slate-700 mt-6 flex justify-center'>
